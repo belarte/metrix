22af78b93571b246e8eca03643105973cd40b3e0
Bruno Belarte <bruno.belarte@gmail.com>
2024-01-04 20:45:08 +0100

    create Manage page

--- server/router.go
+++ server/router.go
@@ -6,16 +6,18 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    t, err := template.ParseFiles("server/templates/main.tmpl", "server/templates/home.tmpl")
-    if err != nil {
-        log.Fatal(err)
+func handler(templateFileName string) func(http.ResponseWriter, *http.Request) {
+    return func(w http.ResponseWriter, r *http.Request) {
+        t, err := template.ParseFiles("server/templates/main.tmpl", "server/templates/"+templateFileName)
+        if err != nil {
+            log.Fatal(err)
+        }
+        t.Execute(w, nil)
     }
-
-    t.Execute(w, nil)
 }
 
 func Run(addr string) {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/", handler("home.tmpl"))
+    http.HandleFunc("/manage", handler("manage.tmpl"))
     log.Fatal(http.ListenAndServe(addr, nil))
 }
