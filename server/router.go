2631d9c9cdc00461661204bde1a854f2949afb8d
Bruno Belarte <bruno.belarte@gmail.com>
2024-01-08 21:48:17 +0100

    manage handler becomes a struct

--- server/router.go
+++ server/router.go
@@ -22,80 +22,88 @@ func homeHandler(c echo.Context) error {
 	})
 }
 
-func manageHandler(db *database.InMemory) func(echo.Context) error {
-	return func(c echo.Context) error {
-		metrics, err := db.GetMetrics()
-		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
+type ManageHandler struct {
+	db *database.InMemory
+}
 
-		return c.Render(http.StatusOK, "page", templateParams{
-			Metrics:  metrics,
-			Selected: database.Metric{},
-			Content:  "manage",
-		})
+func NewManageHandler(db *database.InMemory) *ManageHandler {
+	return &ManageHandler{
+		db: db,
 	}
 }
 
-func clickHandler(db *database.InMemory) func(echo.Context) error {
-	return func(c echo.Context) error {
-		var metric database.Metric
-		if err := c.Bind(&metric); err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
-		}
-
-		metric, err := db.AddMetric(metric)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
-
-		metrics, err := db.GetMetrics()
-		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
-
-		return c.Render(http.StatusOK, "manage", templateParams{
-			Metrics:  metrics,
-			Selected: metric,
-		})
+func (handler *ManageHandler) Manage(c echo.Context) error {
+	metrics, err := handler.db.GetMetrics()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
 	}
+
+	return c.Render(http.StatusOK, "page", templateParams{
+		Metrics:  metrics,
+		Selected: database.Metric{},
+		Content:  "manage",
+	})
 }
 
-func selectHandler(db *database.InMemory) func(echo.Context) error {
-	return func(c echo.Context) error {
-		val := c.FormValue("manage-select")
-
-		id, err := strconv.Atoi(c.FormValue("manage-select"))
-		if err != nil {
-			return c.String(http.StatusBadRequest, err.Error())
-		}
-		log.Printf("val: %s - id: %d", val, id)
-
-		metric, err := db.GetMetric(id)
-		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
-
-		metrics, err := db.GetMetrics()
-		if err != nil {
-			return c.String(http.StatusInternalServerError, err.Error())
-		}
-
-		return c.Render(http.StatusOK, "manage", templateParams{
-			Metrics:  metrics,
-			Selected: metric,
-		})
+func (handler *ManageHandler) Submit(c echo.Context) error {
+	var metric database.Metric
+	if err := c.Bind(&metric); err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
 	}
+
+	metric, err := handler.db.AddMetric(metric)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	metrics, err := handler.db.GetMetrics()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.Render(http.StatusOK, "manage", templateParams{
+		Metrics:  metrics,
+		Selected: metric,
+	})
+}
+
+func (handler *ManageHandler) Select(c echo.Context) error {
+	val := c.FormValue("manage-select")
+
+	id, err := strconv.Atoi(c.FormValue("manage-select"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	log.Printf("val: %s - id: %d", val, id)
+
+	metric, err := handler.db.GetMetric(id)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	metrics, err := handler.db.GetMetrics()
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.Render(http.StatusOK, "manage", templateParams{
+		Metrics:  metrics,
+		Selected: metric,
+	})
 }
 
 func Run(addr string, db *database.InMemory) error {
+	manageHandler := NewManageHandler(db)
 	e := echo.New()
+
 	e.Renderer = NewTemplateRenderer()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+
 	e.GET("/", homeHandler)
-	e.GET("/manage", manageHandler(db))
-	e.POST("/manage/click", clickHandler(db))
-	e.GET("/manage/select", selectHandler(db))
+	e.GET("/manage", manageHandler.Manage)
+	e.POST("/manage/submit", manageHandler.Submit)
+	e.GET("/manage/select", manageHandler.Select)
+
 	return e.Start(addr)
 }
