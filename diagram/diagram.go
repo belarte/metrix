22d009a19c1dfd2e55c9cb5891382fe126695409
Bruno Belarte <bruno.belarte@gmail.com>
2024-01-15 20:04:26 +0100

    create diagram package

---
+++ diagram/diagram.go
@@ -0,0 +1,39 @@
+package diagram
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/belarte/metrix/database"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+)
+
+func graphData(entries []database.Entry) ([]string, []opts.LineData) {
+	var x []string
+	var y []opts.LineData
+
+	for _, e := range entries {
+		x = append(x, e.Date)
+		y = append(y, opts.LineData{Value: e.Value})
+	}
+
+	return x, y
+}
+
+func DataToGraph(metric database.Metric, entries []database.Entry) string {
+	line := charts.NewLine()
+	line.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
+		Title:    metric.Title,
+		Subtitle: metric.Description,
+	}))
+	x, y := graphData(entries)
+	label := fmt.Sprintf("%s (%s)", metric.Title, metric.Unit)
+	line.SetXAxis(x).AddSeries(label, y)
+	buff := new(bytes.Buffer)
+	line.Render(buff)
+	lines := bytes.Split(buff.Bytes(), []byte("\n"))
+	lines = lines[10 : len(lines)-8]
+	s := string(bytes.Join(lines, []byte("\n")))
+	return s
+}
--- server/reports_handler.go
+++ server/reports_handler.go
@@ -1,14 +1,11 @@
 package server
 
 import (
-	"bytes"
-	"fmt"
 	"net/http"
 	"strconv"
 
 	"github.com/belarte/metrix/database"
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/belarte/metrix/diagram"
 	"github.com/labstack/echo/v4"
 )
 
@@ -34,21 +31,9 @@ func (h *ReportsHandler) Reports(c echo.Context) error {
 	})
 }
 
-func graphData(entries []database.Entry) ([]string, []opts.LineData) {
-	var x []string
-	var y []opts.LineData
-
-	for _, e := range entries {
-		x = append(x, e.Date)
-		y = append(y, opts.LineData{Value: e.Value})
-	}
-
-	return x, y
-}
-
 func (h *ReportsHandler) Select(c echo.Context) error {
 	val := c.FormValue("reports-select")
-	if val == "select-reports" {
+	if val == "reports-select" {
 		return c.String(http.StatusOK, "Please select a metric.")
 	}
 
@@ -67,21 +52,6 @@ func (h *ReportsHandler) Select(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	line := charts.NewLine()
-	line.SetGlobalOptions(charts.WithTitleOpts(opts.Title{
-		Title:    metric.Title,
-		Subtitle: metric.Description,
-	}))
-
-	x, y := graphData(entries)
-	label := fmt.Sprintf("%s (%s)", metric.Title, metric.Unit)
-	line.SetXAxis(x).AddSeries(label, y)
-
-	buff := new(bytes.Buffer)
-	line.Render(buff)
-	lines := bytes.Split(buff.Bytes(), []byte("\n"))
-	lines = lines[10 : len(lines)-8]
-	s := string(bytes.Join(lines, []byte("\n")))
-
+	s := diagram.DataToGraph(metric, entries)
 	return c.String(http.StatusOK, s)
 }
