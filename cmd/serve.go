b5cfbbcdcd349f39e53ff27a062df1a3173a8232
Bruno Belarte <bruno.belarte@gmail.com>
2024-02-14 22:10:33 +0100

    add command line options to serve command

--- cmd/serve.go
+++ cmd/serve.go
@@ -12,7 +12,10 @@ var serverCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Start the server",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		db, err := repository.New(":memory:")
+		addr := cmd.Flag("address").Value.String()
+		dbPath := cmd.Flag("database").Value.String()
+
+		db, err := repository.New(dbPath)
 		if err != nil {
 			return fmt.Errorf("could not create database: %w", err)
 		}
@@ -22,10 +25,13 @@ var serverCmd = &cobra.Command{
 			return fmt.Errorf("could not migrate database: %w", err)
 		}
 
-		return server.New(server.WithRepository(db), server.WithAddress(":8080")).Start()
+		return server.New(server.WithRepository(db), server.WithAddress(addr)).Start()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(serverCmd)
+
+	serverCmd.Flags().StringP("address", "a", ":8080", "address to listen to")
+	serverCmd.Flags().StringP("database", "d", ":memory:", "path to the database")
 }
